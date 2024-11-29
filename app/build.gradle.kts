f3a89e8e3129e146fe44603932258e70702ca657
Mainamanhoon <gour.aman07@gmail.com>
2024-11-29 20:43:34 +0530

    introActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures{
+        viewBinding = true
+    }
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -45,4 +48,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.glide)
 }
